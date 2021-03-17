64bb28252422f6ba9a295d746417255da3bd7bd6
herbertbarbosajr <hebertbarbosajr@gmail.com>
2021-03-17 20:03:49 -0300

    Adicionar arquivos de projeto.

---
+++ BioLab_Cadastro/Models/Conexao.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BioLab_Cadastro.Models
+{
+    public class Conexao : DbContext
+    {
+      public Conexao (DbContextOptions<Conexao> options): base (options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ BioLab_Cadastro/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BioLab_Cadastro.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public int Nome { get; set; }
+
+        [Display(Name = "CPF")]
+        [Column("CPF")]
+        public int CPF { get; set; }
+
+        [Display(Name = "Endereco")]
+        [Column("Endereco")]
+        public int Endereco { get; set; }
+
+        [Display(Name = "Celular")]
+        [Column("Celular")]
+        public int Celular { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public int Email { get; set; }
+
+        
+    }
+}
